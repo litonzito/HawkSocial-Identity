4ddd5ddc4c0438dc99076b08ca7cfdcfe05aa497
Adam Palomares <adam.palomares@cetys.edu.mx>
2025-11-07 20:14:16 -0800

    Repo InitialCreate
    Inicializacion del repositorio con la creacion inicial del proyecto con el modelo basico (POST) declarado tanto en Models/ como en HawkSocialDbContext

---
+++ HawkSocial/Data/ApplicationDbContext.cs
@@ -0,0 +1,39 @@
+﻿using HawkSocial.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace HawkSocial.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // tablas extendidas
+        public DbSet<HawkSocial.Models.Post> Posts { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // POST //
+            builder.Entity<Post>(e =>
+            {
+                e.HasIndex(p=> p.CreatedAtUtc); // indice para consultas por fecha de creacion
+                e.Property(p => p.Content)
+                    .HasMaxLength(140)
+                    .IsRequired();
+
+                // FKs //
+                e.HasOne(p => p.User)
+                    .WithMany()
+                    .HasForeignKey(p => p.UserId)
+                    .IsRequired()
+                    .OnDelete(DeleteBehavior.Cascade);
+            });
+
+        }
+    }
+}
---
+++ HawkSocial/Models/Post.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Data;
+using Microsoft.AspNetCore.Identity;
+
+namespace HawkSocial.Models
+{
+    public class Post
+    {
+        // KEY //
+        public int Id { get; set; }
+
+        // DATA //
+        [Required, MaxLength(140)]
+        public string Content { get; set; } = string.Empty;
+
+        // METADATA //
+        public DateTimeOffset CreatedAtUtc { get; set; } = DateTimeOffset.UtcNow;
+        public DateTimeOffset? UpdatedAtUtc { get; set; }
+        public bool IsEdited { get; set; } = false;
+
+        // FK //
+        [Required]
+        public string UserId { get; set; } = default!;
+        public IdentityUser User { get; set; } = default!;
+    }
+}
